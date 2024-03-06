3e1bba3d3d2d250350fdc4133a1615505d000c49
Evan <120244164+evanneff1@users.noreply.github.com>
2024-03-06 16:37:08 -0700

    Update stoplight.c

--- stoplight.c
+++ stoplight.c
@@ -1,88 +1,99 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <PubSubClient.h>
+
+// WiFi credentials
+const char* ssid = "workstationWifi";
+const char* password = "Zorp7825!"; 
+
+// MQTT Broker settings
+const char* mqtt_server = "192.168.137.199"; 
+const int mqtt_port = 1883;
+const char* mqtt_user = "evan"; 
+const char* mqtt_password = "791313"; 
+
+// LED pins
 const int greenLed = D8;
 const int yellowLed = D7;
-const int redLed = D6;\
-int blinkFreq = 500;
-unsigned long blinkTimer = 0;
+const int redLed = D6;
 
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-volatile bool flashRedLed = false;
+bool doorIsOpen = false; 
 
-// Your WiFi credentials
-// const char* ssid = "AirPhone";
-// const char* password = "BeatsFarm25!";
-// volatile bool cycling = false;
-
-const char* ssid = "HelloThere";
-const char* password = "GeneralKenob1";
-
-
-ESP8266WebServer server(80);  // Create a web server on port 80
-
-void setup() {
+void setupWiFi() {
   Serial.begin(115200);
-  
-  pinMode(redLed, OUTPUT);
-  pinMode(yellowLed, OUTPUT);
-  pinMode(greenLed, OUTPUT);
-
-  server.on("/red", []() {
-  flashRedLed = false;
-  setLights(HIGH, LOW, LOW);
-  server.send(200, "text/html", "<html><body>LED set to Red</body></html>");
-});
-
-server.on("/yellow", []() {
-  flashRedLed = false;
-  setLights(LOW, HIGH, LOW);
-  server.send(200, "text/html", "<html><body>LED set to Yellow</body></html>");
-});
-
-server.on("/green", []() {
-  flashRedLed = false;
-  setLights(LOW, LOW, HIGH);
-  server.send(200, "text/html", "<html><body>LED set to Green</body></html>");
-});
-
-server.on("/flashred", []() {
-  setLights(HIGH, LOW, LOW);
-  flashRedLed = true;  // Start flashing
-  server.send(200, "text/html", "<html><body>Flashing Red LED</body></html>");
-});
-
-server.on("/stopflash", []() {
-  flashRedLed = false;  // Stop flashing
-  // setLights(LOW, LOW, LOW);
-  server.send(200, "text/html", "<html><body>Stopped Flashing</body></html>");
-});
-
-
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("");
-  Serial.print("Connected to WiFi. IP address: ");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
 
-  server.begin();
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message;
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+
+  if (String(topic) == topicDoor) {
+    doorIsOpen = (message == "opened");
+  }
+
+  if (doorIsOpen) {
+    if (String(topic) == topicRed) {
+      setLights(HIGH, LOW, LOW);
+    } else if (String(topic) == topicYellow) {
+      setLights(LOW, HIGH, LOW);
+    } else if (String(topic) == topicGreen) {
+      setLights(LOW, LOW, HIGH);
+    } else if (String(topic) == topicFlashRed) {
+      setLights(LOW, LOW, LOW);
+      flashRedLed = true;
+    }
+  } else {
+    setLights(LOW, LOW, LOW);
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("StoplightClient", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe("distance/status");
+      client.subscribe("home/door"); 
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  setupWiFi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  pinMode(redLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
 }
 
 void loop() {
-  server.handleClient();
-  if (flashRedLed) {
-    unsigned long currentMillis = millis();
-    if (currentMillis - blinkTimer >= blinkFreq) {
-      blinkTimer = currentMillis;  // Reset the timer
-
-      // Toggle the red LED
-      int ledState = digitalRead(redLed);  // Get the current state of the red LED
-      digitalWrite(redLed, !ledState);  // Set the LED to the opposite state
-    }
+  if (!client.connected()) {
+    reconnect();
   }
+  client.loop();
 }
 
 void setLights(bool red, bool yellow, bool green) {
